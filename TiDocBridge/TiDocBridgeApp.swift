92db55ea3bb5814185600d0e7c275159cb9075be
Marie Rigal <marierigal4@gmail.com>
2026-09-21 12:08:39 +0200

    Initial Commit

---
+++ TiDocBridge/TiDocBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiDocBridgeApp.swift
+//  TiDocBridge
+//
+//  Created by Marie Rigal on 21/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TiDocBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TiDocBridgeTests/TiDocBridgeTests.swift
@@ -0,0 +1,19 @@
+//
+//  TiDocBridgeTests.swift
+//  TiDocBridgeTests
+//
+//  Created by Marie Rigal on 21/09/2026.
+//
+
+import Testing
+@testable import TiDocBridge
+
+struct TiDocBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
